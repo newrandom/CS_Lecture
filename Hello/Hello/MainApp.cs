84e1485bcc858f86e03def771956aa36121236f5
Koong_i's github <mybac1234@naver.com>
2022-08-25 15:43:45 +0900

    Initial commit

---
+++ Hello/Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)       // if args의 길이가 0일때, 즉 그냥 실행시켰을때의 조건문
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");  // 파일명 + args의 이용방법을 안내
+                return;         // return으로 프로그램 종료
+            }
+                Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
