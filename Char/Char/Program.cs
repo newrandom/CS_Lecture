8a51544cde224f7efffe2605bf4b03cae0ec22df
Koong_i's github <mybac1234@naver.com>
2022-08-26 20:21:30 +0900

    lecture upload

--- Char/Char/Program.cs
+++ Char/Char/Program.cs
@@ -6,7 +6,20 @@ namespace Char
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write(a);       // Console.Write : 데이터를 출력 후에 줄을 바꾸지 않음.
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+
+
         }
     }
 }
---
+++ String/String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요?";
+            string b = "박상현입니다.";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
+
