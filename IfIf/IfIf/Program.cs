8df94471b002c1a85996b87cd841824a9b02a78e
Koong_i's github <mybac1234@naver.com>
2022-09-02 00:57:49 +0900

    lecture upload

---
+++ IfIf/IfIf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하세요 : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            // int number = Int32.Parse(input);
+
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("0보타 큰 짝수.");
+                else
+                    Console.WriteLine("0보타 큰 홀수.");
+            }
+            else
+            {
+                Console.WriteLine("0보타 작거나 같은 수.");
+            }
+        }
+    }
+}
+
