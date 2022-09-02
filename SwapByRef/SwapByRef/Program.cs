f2d6c9ad0407bcd52491d54b6fa436bc7aba4f16
Koong_i's github <mybac1234@naver.com>
2022-09-03 02:18:52 +0900

    lecture upload

---
+++ SwapByRef/SwapByRef/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwapByRef
+{
+    class MainApp
+    {
+        static void Swap(ref int a , ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            //Swap(x, y);     // ref 를 사용하지 않으면 오류가 발생한다.
+            //Console.WriteLine($"x:{x}, y:{y}");
+        }
+    }
+}
