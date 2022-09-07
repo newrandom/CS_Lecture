ed9bcee3af2b09337e8041b504078107afcd4a87
Koong_i's github <mybac1234@naver.com>
2022-09-07 22:37:09 +0900

    upload

---
+++ ReadonlyFields/ReadonlyFields/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReadonlyFields
+{
+    class Configuration
+    {
+        private readonly int min;
+        private readonly int max;
+
+        public Configuration(int v1, int v2)
+        {
+            min = v1;
+            min = v2;
+        }
+
+        public void ChangeMax(int newMax)
+        {
+            max = newMax;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Configuration c = new Configuration(100, 10);
+        }
+    }
+}
