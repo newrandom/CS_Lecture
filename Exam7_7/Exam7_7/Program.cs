a9df9a920aff0f4d46157c2264cec2e791d11944
Koong_i's github <mybac1234@naver.com>
2022-09-14 11:30:58 +0900

    upload

---
+++ Exam7_7/Exam7_7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PositionalPattern
+{
+    class MainApp
+    {
+        //private static double GetDiscountRate(object client)
+        //{
+        //    return client switch
+        //    {
+        //        ("학생", int n) when n < 18 => 0.2,
+        //        ("학생", _) => 0.1,
+        //        ("일반", int n) when n < 18 => 0.1,
+        //        ("일반", _) => 0.05,
+        //        _ => 0,
+        //    };
+        //}
+
+        private static double GetDiscountRate(object client)
+        {
+            switch (client)
+            {
+                case ("학생", int n) when n < 18:
+                    return 0.2;
+                case ("학생", _):
+                    return 0.1;
+                case ("일반", int n) when n < 18:
+                    return 0.1;
+                case ("일반", _):
+                    return 0.05;
+                default:
+                    return 0;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var alice = (job: "학생", age: 17);
+            var bob = (job: "학생", age: 23);
+
+            Console.WriteLine($"alice   : {GetDiscountRate(alice)}");
+
+            Console.WriteLine($"bob     : {GetDiscountRate(bob)}");
+        }
+    }
+}
